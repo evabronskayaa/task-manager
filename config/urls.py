a36b3b5f60fac30a695d527e447611d6c3747de5
eva <eva27122002b@gmail.com>
2022-08-11 17:57:20 +0500

    added list-items

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('tasks/', include('tasks.urls')),
     path('lists/', include('lists.urls'))
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import serializers, viewsets
 from django.contrib.auth import get_user_model
-from .models import List
+from .models import List, ListItem
 
 
 class ListSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,6 +12,19 @@ class ListSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['name', 'description', 'user', 'created_at', 'updated_at']
 
 
+class ListItemSerializer(serializers.HyperlinkedModelSerializer):
+    parent_list = serializers.PrimaryKeyRelatedField(queryset=List.objects.all())
+
+    class Meta:
+        model = ListItem
+        fields = ['text', 'parent_list', 'created_at', 'updated_at']
+
+
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+
+
+class ListItemViewSet(viewsets.ModelViewSet):
+    queryset = ListItem.objects.all()
+    serializer_class = ListItemSerializer
